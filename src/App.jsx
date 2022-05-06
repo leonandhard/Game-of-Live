65007a73f6be0fdcb7b7cf581be30e8d3e045f3d
Leonhard <leonhard@leonharddembp.home>
2022-05-06 19:08:27 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import produce from 'immer';
 
 const numRows =50;
 const numCols = 50;
-
+const operations = [
+  [0,1],
+  [0,-1],
+  [1,1],
+  [1,-1],
+  [-1,1],
+  [-1,-1],
+  [-1,0],
+  [1,0],
+]
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i <numRows; i++){
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+}
 function App() {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i <numRows; i++){
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
+    return generateEmptyGrid()
   })
   // console.log(grid);
   const [running, setRunning] = useState(false)
@@ -21,14 +33,54 @@ function App() {
     if (!runningRef.current) {
       return;
     }
-    setTimeout(runSimulation,1000)
+
+    setGrid( g =>{
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k =0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x,y]) => {
+              const newI = x + i;
+              const newK = y + k;
+              if (newI >=0 && newI < numRows && newK >=0 && newK <numCols) {
+                neighbors += g[newI][newK]
+              }
+            })
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0; 
+            } else if (g[i][k] === 0 && neighbors ===3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      })
+    })
+    setTimeout(runSimulation,100)
   },[])
   return (
     <>
       <button onClick={() => {
-        setRunning(!running)
+        setRunning(!running);
+        if (!running) {
+          runningRef.current = true
+          runSimulation()
+        }
       }}>
         {running ? 'Stop' : 'Start'}
+      </button>
+      <button onClick={() => {
+        const rows = [];
+        for (let i = 0; i <numRows; i++){
+          rows.push(Array.from(Array(numCols), () => (Math.random() > 0.6 ? 1 : 0)));
+        }
+        setGrid(rows)
+      }}>
+        random state
+      </button>
+      <button onClick={() => {
+        setGrid(generateEmptyGrid());
+      }}>
+        clear
       </button>
       <div style={{
         display:'grid',
