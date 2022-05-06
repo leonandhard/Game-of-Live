9e217cf0a64e22c81d2e0bdf9a7f3ffdd55220fd
Leonhard <leonhard@leonharddembp.home>
2022-05-06 17:15:42 +1000

    new file

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css';
+import React, {useState, useCallback, useRef} from 'react';
+import produce from 'immer';
+
+const numRows =50;
+const numCols = 50;
+
+function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i <numRows; i++){
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  })
+  // console.log(grid);
+  const [running, setRunning] = useState(false)
+  const runningRef = useRef(running)
+  runningRef.current = running
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    setTimeout(runSimulation,1000)
+  },[])
+  return (
+    <>
+      <button onClick={() => {
+        setRunning(!running)
+      }}>
+        {running ? 'Stop' : 'Start'}
+      </button>
+      <div style={{
+        display:'grid',
+        gridTemplateColumns: `repeat(${numCols},20px)`
+      }}>
+        {grid.map((rows,i) => 
+          rows.map((col,k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k]? 0 : 1;
+                })
+                setGrid(newGrid)
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? 'pink' : undefined,
+                border: 'solid 1px black'
+              }}
+            
+            />
+          )
+        )
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
